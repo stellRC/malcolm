57a90b9374ad4d12556450d6671bdb7021d37b7b
stellRC <38394781+stellRC@users.noreply.github.com>
2022-07-20 18:07:36 -0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -6,11 +6,16 @@ const navLink = document.querySelectorAll('.nav-link')
 const books = document.querySelectorAll('.book-purchase')
 const bookInnerTops = document.querySelectorAll('.book-inner-top')
 const navBar = document.querySelector('#main')
+const mainSections = document.querySelectorAll('main section')
+const main = document.querySelector('main')
 const topOfNav = navBar.offsetTop;
 
+const navBarHeight = navBar.offsetHeight;
+document.documentElement.style.setProperty('--scroll-padding', navBarHeight - 1 + 'px');
 
 hamburger.addEventListener('click', () => {
     mobileNav.classList.toggle('show')
+    navBar.classList.toggle('expand')
     mobileNav.classList.toggle('translate')
     hamburgerSpan.classList.toggle('hamburger-active')
     hamburgerSpanReverse.classList.toggle('hamburger-active-span')
@@ -18,9 +23,24 @@ hamburger.addEventListener('click', () => {
 
 navLink.forEach(link => link.addEventListener('click', () => {
     mobileNav.classList.toggle('show')
+
     mobileNav.classList.toggle('translate')
     hamburgerSpan.classList.toggle('hamburger-active')
     hamburgerSpanReverse.classList.toggle('hamburger-active-span')
+
+    if (navBar.classList.contains('expand')) {
+        navBar.classList.toggle('expand')
+    }
+
+
+
+    // if (link.classList.contains('underline-active')) {
+    //     link.classList.toggle('underline-active')
+    // } else {
+    //     link.classList.toggle('underline-active')
+    // }
+
+
 }))
 
 // books.forEach(book => book.addEventListener('mouseenter', () => {
@@ -34,16 +54,16 @@ navLink.forEach(link => link.addEventListener('click', () => {
 
 // }))
 
-books.forEach(book => book.addEventListener('mouseenter', () => {
-    book.children[0].classList.toggle('visible')
+// books.forEach(book => book.addEventListener('mouseenter', () => {
+//     book.children[0].classList.toggle('visible')
 
 
-}))
+// }))
 
-books.forEach(book => book.addEventListener('mouseleave', () => {
-    book.children[0].classList.toggle('visible')
+// books.forEach(book => book.addEventListener('mouseleave', () => {
+//     book.children[0].classList.toggle('visible')
 
-}))
+// }))
 
 function fixNav() {
     if (window.scrollY >=topOfNav) {
@@ -52,8 +72,19 @@ function fixNav() {
         document.body.classList.add('fixed-nav');
     } else {
         document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
 
     }
 }
 
 window.addEventListener('scroll', fixNav)
+
+
+function resizeNav() {
+    if (navBar.classList.contains('expand') && window.innerWidth > 1000) {
+        navBar.classList.toggle('expand')
+    }
+}
+
+
+window.onresize = resizeNav;
